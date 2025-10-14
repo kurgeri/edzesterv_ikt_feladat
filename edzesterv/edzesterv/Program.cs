631773c07a6bbbdd4c0d30b73c7a1321967c73b2
kurgeri <b3kurger@vasvari.org>
2025-10-14 12:29:36 +0200

    változok létrehozása

---
+++ edzesterv/edzesterv/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edzesterv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nev;
+            double testsuly, edzesido, kaloria, teljeshetiedzes, kaloriaszoro;
+            byte edzescelja, edzeserroseg;
+        }
+    }
+}
