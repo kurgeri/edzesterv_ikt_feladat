1a91d2a2836ff18db742ba62137c0768c6427c2e
unknown <b3kurger@vasvari.org>
2025-10-17 18:50:23 +0200

    Név bekérésnek kezelése

--- edzesterv/edzesterv/Program.cs
+++ edzesterv/edzesterv/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,35 @@ namespace edzesterv
     {
         static void Main(string[] args)
         {
-            string nev;
-            double testsuly, edzesido, kaloria, teljeshetiedzes, kaloriaszoro;
-            byte edzescelja, edzeserroseg;
-            Console.Write("Adja meg a nevét: ");
-            nev = Convert.ToString(Console.ReadLine());
+
+            // 1. feladat
+            double testsuly, kaloria, teljeshetiedzes, kaloriaszoro;
+            byte edzescelja;
+            int helyesNev = 0;
+            do
+            {
+                helyesNev = 0;
+                Console.Write("Adja meg a nevét: ");
+                string nev = Convert.ToString(Console.ReadLine());
+                if (nev[0].ToString() == nev[0].ToString().ToUpper())
+                {
+                    helyesNev++;
+                }
+                for (int i = 0; i < nev.Length; i++)
+                {
+                    if (nev[i] == ' ')
+                    {
+                        helyesNev++;
+                        if (nev[i + 1].ToString() == nev[i + 1].ToString().ToUpper())
+                        {
+                            helyesNev++;
+                        }
+
+                    }
+
+                }
+            }
+            while (helyesNev != 3);
             // ellenőrzés, idk hogy kéne
             Console.Write("Adja meg a testsúlyát 50-120 kg : ");
             testsuly = Convert.ToDouble(Console.ReadLine());
@@ -22,14 +47,18 @@ namespace edzesterv
                 Console.WriteLine("A megadott érték nem jó!");
 
             }
-            Console.WriteLine("Mi az edzés célja? 1. Állóképesség fejlesztése \n 2. Izomtömeg növelése \n 3.Fogyás");
+
+            //2. feladat
+            Console.Write("Mi az edzés célja? 1. Állóképesség fejlesztése \n 2. Izomtömeg növelése \n 3.Fogyás");
             edzescelja = Convert.ToByte(Console.ReadLine());
-           
+            double edzesido = 0;
             switch (edzescelja) {
 
+                default:
+                Console.WriteLine("Érvénytelen érték!");
+                break;
                 case 1:
-                    
-                   edzesido = 45;
+                    edzesido = 45;
                     break;
                 case 2:
                     edzesido = 60;
@@ -37,21 +66,17 @@ namespace edzesterv
                 case 3:
                     edzesido = 30;
                     break;
-                default:
-                    Console.WriteLine("Érvénytelen érték!");
-                    edzesido = 0;
-                    break;
-                
-                    
-
-            
-            
-            
-            
+               
             }
 
+            // 3.feladat 
+            Console.Write("Adja meg hány napot szeretne edzeni (1-7):");
+            byte edzesnap = Convert.ToByte(Console.ReadLine());
+            Console.Write("Adjon meg egy erősségi szintet(1-5): ");
+            byte edzesero = Convert.ToByte(Console.ReadLine());
+            double edzesidonap = edzesido * (1 + 0.1 * edzesero);
 
-
+            
 
         }
     }
